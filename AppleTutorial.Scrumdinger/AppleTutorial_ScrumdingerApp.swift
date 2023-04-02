766c5f87c9465079d5be417c7c0c60c79af7b989
shiryu.sasaki <shiryu.sasaki@gmail.com>
2023-04-02 14:06:46 +0900

    Initial Commit

---
+++ AppleTutorial.Scrumdinger/AppleTutorial_ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleTutorial_ScrumdingerApp.swift
+//  AppleTutorial.Scrumdinger
+//
+//  Created by 後藤 子龍 on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct AppleTutorial_ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
