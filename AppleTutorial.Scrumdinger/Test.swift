8b88c6a526ab21ddba46c115531ddc8be0621c90
shiryu.sasaki <shiryu.sasaki@gmail.com>
2023-04-06 23:18:58 +0900

    test

---
+++ AppleTutorial.Scrumdinger/Test.swift
@@ -0,0 +1,20 @@
+//
+//  Test.swift
+//  AppleTutorial.Scrumdinger
+//
+//  Created by 後藤 子龍 on 2023/04/06.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text("これなら反映されるだろうか")
+    }
+}
+
+struct Test_Previews: PreviewProvider {
+    static var previews: some View {
+        Test()
+    }
+}
